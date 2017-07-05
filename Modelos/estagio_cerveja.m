dd030288360282bc95ae19e8778ba4a67783cc65
Rafael Borges <Rafael Borges>
2017-07-05 16:58:59 +0100

    modelos atualizados

--- Modelos/estagio_cerveja.m
+++ Modelos/estagio_cerveja.m
@@ -13,30 +13,39 @@ beer = data.beer;
 fridge = data.fridge;
 sala = data.sala;
 
-% 1st order model
+% modelo aquecimento
 
-Phi = [beer(14477:end-1)-fridge(14477:end-1) beer(14477:end-1)];
-Y = beer(14478:end);
+Phi = [beer(8925:end-1)-fridge(8925:end-1) beer(8925:end-1)];
+Y = beer(8926:end);
 
 Theta = inv(Phi'*Phi)*Phi'*Y;
 
 K_loss = -Theta(1)/Ts;
 
+% modelo arrefecimento
+
+Phi = [beer(1:8925-1)-fridge(1:8925-1) beer(1:8925-1)];
+Y = beer(2:8925);
+
+Theta = inv(Phi'*Phi)*Phi'*Y;
+
+K_loss_a = -Theta(1)/Ts;
+
 % arrefecimento
-t=1:14477;
-p = polyfit(t',fridge(1:14477),2);
-
-tf_1 = 25;
-tf_2 = 18;
-
-arrefece = polyval(p,-15000:20000);
-
-tmp1 = abs(arrefece-tf_1);
-[idx1 idx1] = min(tmp1);
-closest1 = arrefece(idx1);
-
-tmp2 = abs(arrefece-tf_2);
-[idx2 idx2] = min(tmp2);
-closest2 = arrefece(idx2);
-
-delta = (idx2-idx1)*10.5;
+% t=1:14477;
+% p = polyfit(t',fridge(1:14477),2);
+% 
+% tf_1 = 25;
+% tf_2 = 18;
+% 
+% arrefece = polyval(p,-15000:20000);
+% 
+% tmp1 = abs(arrefece-tf_1);
+% [idx1 idx1] = min(tmp1);
+% closest1 = arrefece(idx1);
+% 
+% tmp2 = abs(arrefece-tf_2);
+% [idx2 idx2] = min(tmp2);
+% closest2 = arrefece(idx2);
+% 
+% delta = (idx2-idx1)*10.5;
