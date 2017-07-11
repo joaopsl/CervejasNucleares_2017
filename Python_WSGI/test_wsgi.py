f04a6239c5e9960be8715bfeaf203ea34c81dd62
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2017-07-11 15:38:23 +0100

    Added python file

---
+++ Python_WSGI/test_wsgi.py
@@ -0,0 +1,24 @@
+import epics
+
+def application(environ,start_response):
+    value  = epics.caget('BeerPi:Tbeer')
+    status = '200 OK'
+    html = '<html>\n' \
+	   '<head>' \
+           '<title>IST Beer Pi</title>' \
+           '<meta http-equiv="refresh" content="10" />' \
+    	   '</head>\n' \
+           '<body>\n' \
+           '<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">\n' \
+           'mod_wsgi Test Page:\n' \
+           '</div>\n' \
+           '<div style="width: 100%; font-size: 30px; font-weight: bold; text-align: center;">\n' \
+           'Beer Temperature: ' + str(value)+ ' Deg\n' \
+           '</div>\n' \
+           '</body>\n' \
+           '</html>\n'
+    response_header = [('Content-type','text/html')]
+    start_response(status,response_header)
+    return [html]
+
+
