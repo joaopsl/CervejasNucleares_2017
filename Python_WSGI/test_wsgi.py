2fbe27b3f537562ebc102b3c2d018eb18b1f73b9
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2017-07-11 16:42:59 +0100

    Titulo e cenas

--- Python_WSGI/test_wsgi.py
+++ Python_WSGI/test_wsgi.py
@@ -1,9 +1,12 @@
 import epics
 
+from time import *
+
 def application(environ,start_response):
     value  = epics.caget('BeerPi:Tbeer')
     value2 = epics.caget('BeerPi:Tfrigo')
     value3 = epics.caget('BeerPi:Text')
+
     status = '200 OK'
     html = '<html>\n' \
 	   '<head>' \
@@ -12,15 +15,17 @@ def application(environ,start_response):
     	   '</head>\n' \
            '<body>\n' \
            '<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">\n' \
-           'mod_wsgi Test Page:\n' \
+           'BeerPi Project Real-time Temperatures:\n' \
            '</div>\n' \
            '<div style="width: 100%; font-size: 30px; font-weight: bold; text-align: center;">\n' \
-           'Beer Temperature: ' + str(value)+ ' Deg\n' \
+           'Beer: ' + str(value)+ ' Deg\n' \
 	   '<div style="width: 100%; font-size: 30px; font-weight: bold; text-align: center;">\n' \
-           'Fridge Temperature: ' + str(value2)+ ' Deg\n' \
+           'Fridge: ' + str(value2)+ ' Deg\n' \
 	   '<div style="width: 100%; font-size: 30px; font-weight: bold; text-align: center;">\n' \
-           'Outside Temperature: ' + str(value3)+ ' Deg\n' \
+           'Outside: ' + str(value3)+ ' Deg\n' \
            '</div>\n' \
+	   '<div style="width: 100%; font-size: 20px; text-align: center;">\n' \
+            str(ctime())+'\n' \
            '</body>\n' \
            '</html>\n'
     response_header = [('Content-type','text/html')]
