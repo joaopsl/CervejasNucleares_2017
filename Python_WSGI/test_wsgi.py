4362b8314aa92a2ce257b48de66fb32809b39467
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2017-07-11 16:26:45 +0100

    Mais temperaturas

--- Python_WSGI/test_wsgi.py
+++ Python_WSGI/test_wsgi.py
@@ -2,6 +2,8 @@ import epics
 
 def application(environ,start_response):
     value  = epics.caget('BeerPi:Tbeer')
+    value2 = epics.caget('BeerPi:Tfrigo')
+    value3 = epics.caget('BeerPi:Text')
     status = '200 OK'
     html = '<html>\n' \
 	   '<head>' \
@@ -14,6 +16,10 @@ def application(environ,start_response):
            '</div>\n' \
            '<div style="width: 100%; font-size: 30px; font-weight: bold; text-align: center;">\n' \
            'Beer Temperature: ' + str(value)+ ' Deg\n' \
+	   '<div style="width: 100%; font-size: 30px; font-weight: bold; text-align: center;">\n' \
+           'Fridge Temperature: ' + str(value2)+ ' Deg\n' \
+	   '<div style="width: 100%; font-size: 30px; font-weight: bold; text-align: center;">\n' \
+           'Outside Temperature: ' + str(value3)+ ' Deg\n' \
            '</div>\n' \
            '</body>\n' \
            '</html>\n'
