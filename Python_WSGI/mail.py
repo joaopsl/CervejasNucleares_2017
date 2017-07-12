a7aaa26b77c0b54ea29b3078d31757f1ccbbdc70
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2017-07-12 11:49:18 +0100

    Py refinado

--- Python_WSGI/mail.py
+++ Python_WSGI/mail.py
@@ -1,3 +1,4 @@
+
 import smtplib
 import time
 import epics
@@ -11,14 +12,17 @@ while True:
     Tbeer_min = Tref - 1.5
     Tbeer_max = Tref + 1.5
     if Tbeer>Tbeer_max:
-	send = 1
-	msg = "WARNING: Beer temperature is too high! Current value: %f DegC
+        send = 1
+        msg = "ALARM: Beer temperature is too high! Current value: %f DegC is %f DegC above the reference value. " %(Tbeer,Tbeer-Tref)
+    if Tbeer<Tbeer_min:
+        send = 1
+        msg = "ALARM: Beer temperature is too low! Current value: %f DegC is %f DegC below the reference value. " %(Tbeer,Tref-Tbeer)
+    if send == 1:
         try:
             server.ehlo()
             server.starttls()
             server.ehlo
             server.login("cervejasnucleares2017@gmail.com","fermentacao2017")
-            msg = "Hello!"
             server.sendmail("cervejasnucleares2017@gmail.com","henriquegs96@hotmail.com", msg)
             print "Mail enviado"
         except:
